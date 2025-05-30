db86eb6c93f6d738e1046a6b8595829e30af8baa
Mark Kurtz <mark.j.kurtz@gmail.com>
2025-05-30 13:39:01 +0000

    Stashing state for working base speculators config

---
+++ src/speculators/models/__init__.py
@@ -0,0 +1,9 @@
+from .independent import IndependentSpeculatorConfig
+from .mlp import MLPSpeculatorConfig
+from .transformer import TransformerSpeculatorConfig
+
+__all__ = [
+    "IndependentSpeculatorConfig",
+    "MLPSpeculatorConfig",
+    "TransformerSpeculatorConfig",
+]
---
+++ src/speculators/models/mlp.py
@@ -0,0 +1,71 @@
+from pydantic import Field
+
+from speculators.config import SpeculatorModelConfig
+
+__all__ = ["MLPSpeculatorConfig"]
+
+
+class MLPSpeculatorConfig(SpeculatorModelConfig):
+    """
+    TODO
+    """
+
+    architectures: list[str] = Field(
+        default_factory=lambda: ["MLPSpeculator"],
+        description=("The architectures this speculator uses."),
+    )
+    torch_dtype: str = Field(
+        default="bfloat16",
+        description=(
+            "The torch dtype this speculator uses. "
+            "This is used to set the dtype of the model."
+        ),
+    )
+    inputs: list[str] = Field(
+        default_factory=lambda: ["input_embeddings", "hidden_states[-1]"],
+        description=(
+            "The inputs from the verifier that this speculator uses to generate "
+            "proposal tokens for verification."
+        ),
+    )
+    inputs_hidden_states_normalized: bool = Field(
+        default=False,
+        description=(
+            "Whether to use the hidden states of the verifier after the layer norm is "
+            "applied. If False, the hidden states are used before the "
+            "layer norm is applied."
+        ),
+    )
+    hidden_size: int = Field(
+        default=4096,
+        description=("The hidden size from the verifier that this speculator targets."),
+    )
+    intermediate_size: int = Field(
+        default=4096,
+        description=(
+            "The intermediate size the MLP speculator uses for predicting tokens from."
+        ),
+    )
+    vocab_size: int = Field(
+        default=128256,
+        description=(
+            "The size of the vocabulary the MLP speculator supports for "
+            "predicting tokens from."
+        ),
+    )
+    num_layers: int = Field(
+        default=5,
+        description=(
+            "The number of layers in the MLP speculator which ties directly to the "
+            "maximum number of tokens the speculator can predict."
+        ),
+    )
+    tie_weights: bool = Field(
+        default=True,
+        description=(
+            "Whether to tie the weights across all of the MLP layers together so they "
+            "are shared. Reduces the overall number of parameters in the model. "
+            "If False, each layer will have its own set of embeddings, linear weights, "
+            "and head weights."
+        ),
+    )
