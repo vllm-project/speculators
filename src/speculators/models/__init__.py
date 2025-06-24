89aee13318de0d397962228a0c4164883b91ce3c
Mark Kurtz <mark.j.kurtz@gmail.com>
2025-06-24 20:40:30 +0000

    Rework the commits and diff to focus this just on the implementation for the Eagle config

--- src/speculators/models/__init__.py
+++ src/speculators/models/__init__.py
@@ -1,12 +1,9 @@
 from .eagle import EagleSpeculatorConfig
 from .independent import IndependentSpeculatorConfig
-from .llama_eagle import LlamaEagleSpeculator, LlamaEagleSpeculatorConfig
 from .mlp import MLPSpeculatorConfig
 
 __all__ = [
     "EagleSpeculatorConfig",
     "IndependentSpeculatorConfig",
-    "LlamaEagleSpeculator",
-    "LlamaEagleSpeculatorConfig",
     "MLPSpeculatorConfig",
 ]
