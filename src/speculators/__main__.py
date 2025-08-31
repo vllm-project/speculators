4bab3362606b3852aaadfa657d2e1f9acdd19ca5
Mark Kurtz <mark.kurtz@neuralmagic.com>
2025-08-31 17:55:34 -0400

    Add base converter implementation with registry and adapt entrypoints/CLI to utilize it
    Signed-off-by: Mark Kurtz <mark.kurtz@neuralmagic.com>

--- src/speculators/__main__.py
+++ src/speculators/__main__.py
@@ -136,7 +136,8 @@ def convert(
             --verifier "meta-llama/Llama-3.1-8B-Instruct"
 
         # eagle3 with normalization before the residual
-            --algorithm-kwargs '{"norm_before_residual": true}' \
+        speculators convert "./eaglev3/checkpoint" \
+            --algorithm-kwargs '{"norm_before_residual": true}'
             --verifier "meta-llama/Llama-3.1-8B-Instruct"
 
     :param model: Model checkpoint path or Hugging Face model ID to convert
