c97a83afa978721562c8c2cc25a97119f58ccb3e
Mark Kurtz <mark.kurtz@neuralmagic.com>
2025-09-04 16:06:55 -0700

    Fix issue from rebase
    Signed-off-by: Mark Kurtz <mark.kurtz@neuralmagic.com>

--- src/speculators/convert/converters/base.py
+++ src/speculators/convert/converters/base.py
@@ -30,7 +30,7 @@ ModelT = TypeVar("ModelT", bound=SpeculatorModel)
 """Generic type variable for speculator models"""
 
 
-class SpeculatorConverter(ABC, Generic[ConfigT, ModelT], RegistryMixin):
+class SpeculatorConverter(ABC, RegistryMixin, Generic[ConfigT, ModelT]):
     """
     Abstract base converter for transforming external checkpoints to Speculators format.
 
