c7a0c3250213788181ae27fcb43cf63e8b053bc7
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2025-10-01 13:34:59 -0400

    Add Eagle3DraftModel implementation
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

---
+++ src/speculators/train/eagle3/model_definitions.py
@@ -0,0 +1,100 @@
+from typing import NamedTuple, Optional
+import torch
+from transformers.configuration_utils import PretrainedConfig
+import copy
+from transformers import Cache, LlamaConfig
+from transformers.models.llama.modeling_llama import (
+    LlamaDecoderLayer,
+    LlamaRMSNorm,
+    LlamaRotaryEmbedding,
+)
+
+from transformers.processing_utils import Unpack
+from transformers.utils.generic import TransformersKwargs
+
+
+class LlamaConcatInputDecoderLayer(LlamaDecoderLayer):
+    def __init__(self, config: PretrainedConfig, layer_idx: int):
+        super().__init__(config, layer_idx)
+
+        ##### CHANGES START #####
+        if layer_idx == 0:
+            self.self_attn.q_proj = torch.nn.Linear(
+                2 * config.hidden_size,  # previous: config.hidden_size
+                config.num_attention_heads * config.head_dim,
+                bias=config.attention_bias,
+            )
+            self.self_attn.k_proj = torch.nn.Linear(
+                2 * config.hidden_size,  # previous: config.hidden_size
+                config.num_key_value_heads * config.head_dim,
+                bias=config.attention_bias,
+            )
+            self.self_attn.v_proj = torch.nn.Linear(
+                2 * config.hidden_size,  # previous: config.hidden_size
+                config.num_key_value_heads * config.head_dim,
+                bias=config.attention_bias,
+            )
+            self.input_layernorm = LlamaRMSNorm(
+                2 * config.hidden_size,  # previous: config.hidden_size
+                eps=config.rms_norm_eps,
+            )
+        ##### CHANGES END #####
+
+    def forward(
+        self,
+        hidden_states: torch.Tensor,
+        attention_mask: Optional[torch.Tensor] = None,
+        position_ids: Optional[torch.LongTensor] = None,
+        past_key_values: Optional[Cache] = None,
+        use_cache: Optional[bool] = False,
+        cache_position: Optional[torch.LongTensor] = None,
+        position_embeddings: Optional[
+            tuple[torch.Tensor, torch.Tensor]
+        ] = None,  # necessary, but kept here for BC
+        **kwargs: Unpack[TransformersKwargs],
+    ) -> torch.Tensor:
+        ##### CHANGES START #####
+        # previous: residual = hidden_states
+        residual = hidden_states[:, :, hidden_states.shape[2] // 2 :]
+        ##### CHANGES END #####
+
+        hidden_states = self.input_layernorm(hidden_states)
+        # Self Attention
+        hidden_states, _ = self.self_attn(
+            hidden_states=hidden_states,
+            attention_mask=attention_mask,
+            position_ids=position_ids,
+            past_key_values=past_key_values,
+            use_cache=use_cache,
+            cache_position=cache_position,
+            position_embeddings=position_embeddings,
+            **kwargs,
+        )
+        hidden_states = residual + hidden_states
+
+        # Fully Connected
+        residual = hidden_states
+        hidden_states = self.post_attention_layernorm(hidden_states)
+        hidden_states = self.mlp(hidden_states)
+        hidden_states = residual + hidden_states
+        return hidden_states
+
+
+class LlamaConcatInputRotaryEmbedding(LlamaRotaryEmbedding):
+    def __init__(self, config: LlamaConfig, device=None):
+        config = copy.copy(config)
+        config.hidden_size = config.hidden_size * 2
+        super().__init__(config, device)
+
+
+class ModelComponents(NamedTuple):
+    decoder_layer_class: type
+    norm_class: type
+    rotary_emb_class: type
+
+
+model_classes: dict[str, ModelComponents] = {
+    "llama": ModelComponents(
+        LlamaConcatInputDecoderLayer, LlamaRMSNorm, LlamaConcatInputRotaryEmbedding
+    ),
+}
