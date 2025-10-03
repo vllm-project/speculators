426c55128ac9563ff3156cf32e89db7bf4392d3a
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2025-10-03 16:30:55 -0400

    Move train script to scripts/ dir
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

---
+++ scripts/train.py
@@ -0,0 +1,94 @@
+import torch
+from transformers import LlamaConfig
+
+from speculators.train.eagle3.core import Eagle3DraftModel
+from speculators.train.data import Eagle3SampleFileDataset, create_collate_fn
+from speculators.train.distributed_batch_sampler import (
+    MultipackDistributedBatchSamplerV2,
+)
+from torch.utils.data import DataLoader
+
+from speculators.train.utils import maybe_setup_distributed, maybe_destroy_distributed
+from speculators.train.trainer import Trainer
+
+
+local_rank, world_size, rank, is_distributed = maybe_setup_distributed()
+
+
+DEVICE = torch.device(local_rank)
+EPOCHS = 10
+draft_vocab_size = 5000
+verifier_vocab_size = 151936
+hidden_size = 5120
+total_seq_len = 2048
+datapath = "./data"
+verifier_model_name_or_path = "Qwen/Qwen2.5-VL-7B-Instruct"
+
+
+# TEMP MODEL SETUP
+llama_config = LlamaConfig(hidden_size=hidden_size)
+llama_config._attn_implementation = "simple_flex_attention"
+
+d2t_vocab = torch.zeros(draft_vocab_size, dtype=torch.long).to(DEVICE)
+t2d_vocab = (
+    torch.cat(
+        [
+            torch.ones(draft_vocab_size),
+            torch.zeros(llama_config.vocab_size - draft_vocab_size),
+        ]
+    )
+    .to(torch.bool)
+    .to(DEVICE)
+)
+# END TEMP MODEL SETUP
+
+
+draft_model = Eagle3DraftModel(
+    hidden_size=hidden_size,
+    t2d_vocab=t2d_vocab,
+    d2t_vocab=d2t_vocab,
+    decoder_layer_config=llama_config,
+    verifier_vocab_size=verifier_vocab_size,
+    verifier_pad_token_id=151643,
+    num_layers=1,
+    ttt_steps=3,
+)
+
+# draft_model.load_verifier_lm_head(verifier_model_name_or_path) # Doesn't work for Qwen2.5 VL, need better head loading method
+
+dataset = Eagle3SampleFileDataset(datapath=datapath, max_len=total_seq_len)
+batch_sampler = MultipackDistributedBatchSamplerV2(
+    batch_max_length=total_seq_len,
+    lengths=dataset.approx_lengths(),
+    num_replicas=world_size,
+    rank=local_rank,
+)
+train_loader = DataLoader(
+    dataset,
+    batch_sampler=batch_sampler,
+    num_workers=16,
+    pin_memory=True,
+    collate_fn=create_collate_fn(total_seq_len),
+)
+
+
+# todo: make config better
+config = {
+    "num_epochs": EPOCHS,
+    "save_path": "./checkpoints",
+    "lr": 1e-4,
+    "total_seq_len": 4096,
+    "datapath": "./data",
+}
+
+
+trainer = Trainer(
+    draft_model, config, train_loader, None, is_distributed, local_rank, world_size
+)
+trainer.train()
+
+maybe_destroy_distributed()
+
+
+# RUN WITH:
+# CUDA_VISIBLE_DEVICES=0,1,2,3 torchrun --nnodes=1 --nproc_per_node=4  scripts/train.py
