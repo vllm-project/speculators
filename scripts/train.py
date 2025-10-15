7afca7b3561b16fe58871580b72e4abdb563ecc6
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2025-10-15 13:03:39 +0000

    Initialize Eagle3DraftModel from Eagle3SpeculatorConfig
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

--- scripts/train.py
+++ scripts/train.py
@@ -3,6 +3,9 @@ import torch
 from torch.utils.data import DataLoader
 from transformers import LlamaConfig
 
+from speculators.config import SpeculatorsConfig, VerifierConfig
+from speculators.models.eagle3 import Eagle3SpeculatorConfig
+from speculators.proposals.greedy import GreedyTokenProposalConfig
 from speculators.train.data import (
     AddUniformNoise,
     Eagle3SampleFileDataset,
@@ -19,50 +22,52 @@ from speculators.train.utils import maybe_destroy_distributed, maybe_setup_distr
 
 local_rank, world_size, rank, is_distributed = maybe_setup_distributed()
 
-
 DEVICE = torch.device(local_rank)
-EPOCHS = 102
+EPOCHS = 20
 draft_vocab_size = 32000
 total_seq_len = 8192
 datapath = "./data"
 verifier_model_name_or_path = "meta-llama/Llama-3.1-8B-Instruct"
+hidden_size = 4096
+verifier_vocab_size = 128256
+draft_vocab_size = 32000
+norm_before_residual = True
+ttt_steps = 3
 
 
-# TEMP MODEL SETUP
-llama_config = LlamaConfig.from_pretrained(verifier_model_name_or_path)
-hidden_size = llama_config.hidden_size
-verifier_vocab_size = llama_config.vocab_size
-llama_config = LlamaConfig(hidden_size=hidden_size, vocab_size=verifier_vocab_size)
+llama_config = LlamaConfig(hidden_size=hidden_size, num_hidden_layers=1)
 llama_config._attn_implementation = "simple_flex_attention"
 
-# d2t = torch.zeros(draft_vocab_size, dtype=torch.long).to(DEVICE)
-# t2d = (
-#     torch.cat(
-#         [
-#             torch.ones(draft_vocab_size),
-#             torch.zeros(llama_config.vocab_size - draft_vocab_size),
-#         ]
-#     )
-#     .to(torch.bool)
-#     .to(DEVICE)
-# )
+
 d2t = torch.from_numpy(np.load("d2t.npy")).to(DEVICE)
 t2d = torch.from_numpy(np.load("t2d.npy")).to(DEVICE)
 
 setup_metric_logger(loggers="trackio", run_name=None, output_dir="./logs")
 setup_root_logger()
-# END TEMP MODEL SETUP
+
+
+speculator_config = Eagle3SpeculatorConfig(
+    transformer_layer_config=llama_config,
+    draft_vocab_size=draft_vocab_size,
+    norm_before_residual=norm_before_residual,
+    speculators_config=SpeculatorsConfig(
+        algorithm="eagle3",
+        proposal_methods=[
+            GreedyTokenProposalConfig(
+                proposal_type="greedy",
+                speculative_tokens=ttt_steps,
+            )
+        ],
+        default_proposal_method="greedy",
+        verifier=VerifierConfig(
+            name_or_path=verifier_model_name_or_path,
+            architectures=["LlamaForCausalLM"],
+        ),
+    ),
+)
 
 draft_model = Eagle3DraftModel(
-    verifier_model_name_or_path=verifier_model_name_or_path,
-    hidden_size=hidden_size,
-    t2d=t2d,
-    d2t=d2t,
-    decoder_layer_config=llama_config,
-    verifier_vocab_size=verifier_vocab_size,
-    verifier_pad_token_id=None,
-    num_layers=1,
-    ttt_steps=3,
+    config=speculator_config, t2d=t2d, d2t=d2t, ttt_steps=ttt_steps
 )
 
 verifier_lm_head = Eagle3VerifierLMHead(
