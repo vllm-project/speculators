d9cd3fdf72e1405f7c106ca7879a5b07f0c0fc80
shanjiaz <zsjwpianpian@gmail.com>
2025-12-04 17:36:05 +0000

    remove from example scripts
    Signed-off-by: shanjiaz <zsjwpianpian@gmail.com>

--- examples/e2e/qwen3_8b_sharegpt_ultrachat.py
+++ examples/e2e/qwen3_8b_sharegpt_ultrachat.py
@@ -43,14 +43,12 @@ if __name__ == "__main__":
     # Data Generation
     data_gen_args_sharegpt = DataGenArgs(
         train_data_path="sharegpt",
-        max_model_len=TOTAL_SEQ_LEN,
         seq_length=TOTAL_SEQ_LEN,
         turn_dropout=True,  # Turn dropout enabled here
     )
 
     data_gen_args_ultrachat = DataGenArgs(
         train_data_path="ultrachat",
-        max_model_len=TOTAL_SEQ_LEN,
         seq_length=TOTAL_SEQ_LEN,
         turn_dropout=True,  # Turn dropout enabled here
     )
