da204660fe1128e42b8e7fc1eea79ac57525ae7c
shanjiaz <zsjwpianpian@gmail.com>
2026-02-26 20:07:35 +0000

    remove datagen folder
    Signed-off-by: shanjiaz <zsjwpianpian@gmail.com>

--- src/speculators/data_generation/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Data generation utilities for EAGLE-style speculative decoding training."""
-
-from speculators.data_generation.vllm_hidden_states_generator import (
-    VllmHiddenStatesGenerator,
-)
-
-__all__ = ["VllmHiddenStatesGenerator"]
