5e94fc08f3349d90d6bbc8eca49d954f5125a93d
Roderick-Wu <rowu@redhat.com>
2026-07-23 17:26:04 -0400

    dflash from older version
    Signed-off-by: Roderick-Wu <rowu@redhat.com>
    Signed-off-by: Roderick Wu <Roderick-Wu@h100-03.nemg-001.lab.rdu2.dc.redhat.com>
    Signed-off-by: Roderick-Wu <rowu@redhat.com>

--- src/speculators/train/trainer.py
+++ src/speculators/train/trainer.py
@@ -460,7 +460,10 @@ class Trainer:
                 else v
                 for k, v in batch.items()
             }
-            gpu_batch = split_batch_for_sp(gpu_batch, get_sp_rank(), get_sp_size())
+            if getattr(self.model, "_sp_splits_batch", True):
+                gpu_batch = split_batch_for_sp(
+                    gpu_batch, get_sp_rank(), get_sp_size()
+                )
 
             with torch.autocast(
                 self.device_type, dtype=self.config.hidden_states_dtype
@@ -555,7 +558,10 @@ class Trainer:
                 else v
                 for k, v in batch.items()
             }
-            gpu_batch = split_batch_for_sp(gpu_batch, get_sp_rank(), get_sp_size())
+            if getattr(self.model, "_sp_splits_batch", True):
+                gpu_batch = split_batch_for_sp(
+                    gpu_batch, get_sp_rank(), get_sp_size()
+                )
 
             with torch.autocast(
                 self.device_type, dtype=self.config.hidden_states_dtype
