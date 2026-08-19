75c9379d97ed66497597633009aa85c234a89a79
Fynn Schmitt-Ulms <fschmitt@redhat.com>
2026-08-19 04:21:01 +0000

    Add DFlash2 model def
    Signed-off-by: Fynn Schmitt-Ulms <fschmitt@redhat.com>

--- src/speculators/models/dflash/__init__.py
+++ src/speculators/models/dflash/__init__.py
@@ -1,7 +1,9 @@
 from speculators.models.dflash.config import DFlashSpeculatorConfig
 from speculators.models.dflash.core import DFlashDraftModel
+from speculators.models.dflash.dflash2 import DFlash2DraftModel
 
 __all__ = [
+    "DFlash2DraftModel",
     "DFlashDraftModel",
     "DFlashSpeculatorConfig",
 ]
