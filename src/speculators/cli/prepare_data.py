f0eba7e2971f9f5e76f2e01d0214f8a427f7da8c
shanjiaz <zsjwpianpian@gmail.com>
2026-09-21 14:31:13 -0400

    comment
    Signed-off-by: shanjiaz <zsjwpianpian@gmail.com>

--- src/speculators/cli/prepare_data.py
+++ src/speculators/cli/prepare_data.py
@@ -172,7 +172,7 @@ def prepare_data(
         typer.Option(
             "--skip-token-freq/--no-skip-token-freq",
             help=(
-                "Skip computing token frequency distribution if you're not using "
+                "Skip token frequency distribution compute if you're not using "
                 "reduced vocabulary."
             ),
         ),
